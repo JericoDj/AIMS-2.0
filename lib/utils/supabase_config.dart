f8b8d9afbfcfac3409d6ef72341a7dc00eda2e73
jericodejesus <dejesusjerico528@gmail.com>
2026-02-23 19:44:28 +0800

    Refactor: Changed dialog title from "Delete Stock" to "Delete Item".

---
+++ lib/utils/supabase_config.dart
@@ -0,0 +1,9 @@
+class SupabaseConfig {
+  static const String url = 'https://ohgujgddzvajordrexvn.supabase.co';
+  static const String anonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oZ3VqZ2RkenZham9yZHJleHZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MzgwNTAsImV4cCI6MjA4NzQxNDA1MH0.QSNhzkmW54cxYnDyNuTdrQzM-g_F3Ess5l6WH8p32P8';
+  static const String barcodeBucket = 'qrcode';
+  static const String profileBucket = 'profile-images';
+}
+
+// BmLGYOAyvOKC3Z8t supadabase database key
